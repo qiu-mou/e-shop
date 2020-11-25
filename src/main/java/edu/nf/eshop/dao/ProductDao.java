2584dab41568fde865d58617916c85724419a539
coder_qiumou <3010774977@qq.com>
2020-11-25 16:03:13 +0800

    add entity,dao

---
+++ src/main/java/edu/nf/eshop/dao/ProductDao.java
@@ -0,0 +1,11 @@
+package edu.nf.eshop.dao;
+
+import edu.nf.eshop.entity.Product;
+
+public class ProductDao {
+
+    public void save(Product product){
+
+    }
+
+}
---
+++ src/main/java/edu/nf/eshop/entity/Product.java
@@ -0,0 +1,13 @@
+package edu.nf.eshop.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Product {
+
+    private String name;
+    private BigDecimal price;
+
+}
